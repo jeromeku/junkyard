fcee91989d1e536eacef1c40d83482fbb9a01b8d
Ivan Krasilnikov <infnty@gmail.com>
2010-04-15 01:33:02 +0400

    michalski - hw 5

--- yandex-school/2010S/learning-theory/problemset5/code.py
+++ yandex-school/2010S/learning-theory/problemset5/code.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
-import numpy, math
+import numpy
 import matplotlib.pyplot as pyplot
 
-# мера странности - расстояние к линии регрессии
-def nonconformity_measure(train_points, test_point):
-    xs = [x for (x, y) in train_points]
-    ys = [y for (x, y) in train_points]
-
+def conformal_algorithm(train_points, test_point):
+    # построение многочлена f(x)=a+bx+cx^2 по обучающей выборке включая
+    # проверяемую точку
+    xs = [x for (x, y) in train_points] + [test_point[0]]
+    ys = [y for (x, y) in train_points] + [test_point[1]]
+    N = len(xs)
     c, b, a = numpy.polyfit(xs, ys, 2)
 
-    x, y = test_point
-    return abs(a + b*x + c*x*x - y)
-
-def conformal_algorithm(train_points, test_point):
-    points = train_points + [test_point]
-    N = len(points)
-
+    # вычисление меры странности - отклонения y от f(x)=a+bx+cx^2
     alpha = []
     for i in range(N):
-        alpha.append(nonconformity_measure(points[:i] + points[(i+1):], points[i]))
+        x, y = xs[i], ys[i]
+        alpha.append(abs(a + b*x + c*x*x - y))
 
     result = 0.0
     for i in range(N):
@@ -33,22 +29,21 @@ def main():
     sample = zip(sample_x, sample_y)
 
     c, b, a = numpy.polyfit(sample_x, sample_y, 2)
-    print a  # точечное предсказание в x=0
+    print '%.9g + %.9gx + %.9gx^2' % (a, b, c)  # точечное предсказание в x=0
 
     plot_y = []; plot_p = []
-    for i in range(1000):
-        y = i / 2000.0
+    for i in range(5000):
+        y = i / 10000.0
         p = conformal_algorithm(sample, (0.0, y))
         if p >= 0.95:
-            print y, p
+            print y,
         plot_y.append(y); plot_p.append(p)
 
     fig = pyplot.figure()
     fig_plot = fig.add_subplot(111)
     pyplot.xlabel('y')
-    pyplot.ylabel('p(y)')
+    pyplot.ylabel('p')
     fig_plot.plot(plot_y, plot_p)
-    fig_plot.plot(plot_y, plot_p)
-    fig.savefig('figure.pdf')
+    fig.savefig('figure2.pdf')
 
 main()
