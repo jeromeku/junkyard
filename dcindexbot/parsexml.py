d8b9b0d909d7d680904a2be62c9c446b94b5a6be
Ivan Krasilnikov <infnty@gmail.com>
2010-05-30 04:07:51 +0400

    Added "dirs" table.

--- dcindexbot/parsexml.py
+++ dcindexbot/parsexml.py
@@ -11,14 +11,17 @@ def log(msg):
     sys.stderr.flush()
 
 class DcXmlFilesHandler(handler.ContentHandler):
-    def __init__(self, callback):
+    def __init__(self, callback, nextDirIdFunc):
         handler.ContentHandler.__init__(self)
         self.callback = callback
+        self.dirStack = [-1]
+        self.nextDirIdFunc = nextDirIdFunc
 
     def startElement(self, name, attrs):
         if name == u'Directory':
             filename = attrs[u'Name']
-            self.callback(filename, u'dir', 0)
+            self.callback(filename, u'dir', 0, self.dirStack[-1])
+            self.dirStack.append(self.nextDirIdFunc())
         elif name == u'File':
             filename = attrs[u'Name']
             components = filename.split(u'.')
@@ -26,13 +29,12 @@ class DcXmlFilesHandler(handler.ContentHandler):
             if len(components) >= 2 and len(components[-1]) <= 8:
                 ext = components[-1]
             size = int(attrs[u'Size'])
-            self.callback(filename, ext, size)
+            self.callback(filename, ext, size, self.dirStack[-1])
 
-def parse(path, callback):
-    bzfile = bz2.BZ2File(path, 'r')
-    parser = make_parser()
-    parser.setContentHandler(DcXmlFilesHandler(callback))
-    parser.parse(bzfile)
+    def endElement(name):
+        if name == u'Directory':
+            if len(self.dirStack) > 1:
+                self.dirStack.pop()
 
 def main():
     parser = optparse.OptionParser(usage='Usage: %prog <data directory>')
@@ -52,17 +54,23 @@ def main():
     conn = sqlite.Connection(temp_file)
 
     c = conn.cursor()
-    c.execute('CREATE TABLE files (name TEXT, name_low TEXT, ext TEXT, size INTEGER, userid INTEGER)')
-    c.execute('CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, name_low TEXT, seen INTEGER)')
+    c.execute('CREATE TABLE files (name TEXT, name_low TEXT, ext TEXT, size INTEGER, userid INTEGER, dirid INTEGER)')
+    c.execute('CREATE TABLE users (userid INTEGER PRIMARY KEY, name TEXT, name_low TEXT, seen INTEGER)')
+    c.execute('CREATE TABLE dirs (dirid INTEGER PRIMARY KEY, parent INTEGER, name TEXT)')
+    # dirid=-1 means no parent directory
+
     c.execute('CREATE INDEX idx1 ON files (name_low)')
     c.execute('CREATE INDEX idx2 ON files (ext)')
     c.execute('CREATE INDEX idx3 ON files (size)')
-    c.execute('CREATE INDEX idx4 ON files (userid)')
-    c.execute('CREATE INDEX idx5 ON users (name_low)')
+    c.execute('CREATE INDEX idx4 ON users (name_low)')
 
-    user_index = [s.strip().split('\t') for s in file(os.path.join(datadir, 'index'))]
-
-    for userid, (username, seentime, filename) in enumerate(user_index):
+    next_dir_id = [0]
+    def alloc_dir_id():
+        next_dir_id[0] += 1
+        return next_dir_id[0] - 1
+    
+    user_list = [s.strip().split('\t') for s in file(os.path.join(datadir, 'index'))]
+    for userid, (username, seentime, filename) in enumerate(user_list):
         try:
             username.decode('utf-8')
         except:
@@ -73,17 +81,19 @@ def main():
         c.execute('INSERT INTO users VALUES(%s,%s,%s,%s)',
             (userid, username, username_u.lower().encode('utf-8'), seentime))
 
-        def callback(filename_u, ext_u, size):
-            c.execute('INSERT INTO files VALUES(%s,%s,%s,%s,%s)', (
+        def callback(filename_u, ext_u, size, dirid):
+            c.execute('INSERT INTO files VALUES(%s,%s,%s,%s,%s,%s)', (
                 filename_u.encode('utf-8'),
                 filename_u.lower().encode('utf-8'),
                 ext_u.lower().encode('utf-8'),
-                size,
-                userid))
+                size, userid, dirid))
 
         try:
             log('Parsing %s\n' % filename)
-            parse(os.path.join(datadir, filename), callback)
+            bzfile = bz2.BZ2File(os.path.join(datadir, filename), 'r')
+            parser = make_parser()
+            parser.setContentHandler(DcXmlFilesHandler(callback, alloc_dir_id))
+            parser.parse(bzfile)
         except KeyboardInterrupt:
             raise
         except:
